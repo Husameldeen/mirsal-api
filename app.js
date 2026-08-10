083a4252da4e0fe5aabdfc00522771c6c3eb93cb
Husameldeen <mhusam87@gmail.com>
2026-08-10 07:55:55 +0200

    helmet was add to add requests headers and express-rate-limit was added

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import usersRouter from './routes/usersRoute.js';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 100,
+
+  standardHeaders: 'draft-8',
+  legacyHeaders: true, // set false to disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56,
+  message: 'too much request frm thi IP, Please try again in one hour!',
+});
 
 const app = express();
+
+app.use(helmet()); // Set HTTP Security Headers
+app.use('/api', limiter); // Limit requests from same IP
 
 app.use(express.json({ limit: '10kb' })); // Reading  data from body of request
 app.set('query parser', 'extended'); // Reading  data from body of request
