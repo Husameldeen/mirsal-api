fcfa4cd27cd0665eb62fb78fa3493e37b7fd47b6
Husameldeen <mhusam87@gmail.com>
2026-08-10 09:53:22 +0200

    trying to configure why db is not working 2

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -3,7 +3,9 @@ import { driverSignup, userSignup } from '../controllers/authControllers.js';
 
 const usersRouter = express.Router();
 
-usersRouter.post('/user-signup', userSignup);
+usersRouter.post('/user-signup', (req, res, next) => {
+  res.status(200).json({ status: 'success', data: null });
+});
 
 usersRouter.post('/driver-signup', driverSignup);
 
