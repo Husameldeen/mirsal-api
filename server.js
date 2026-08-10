3011d5eb8c354daf6ee0facd1123eee2ddf0ec40
Husameldeen <mhusam87@gmail.com>
2026-08-10 13:02:06 +0200

    trying to configure why db is not working 4

--- server.js
+++ server.js
@@ -19,16 +19,20 @@ console.log('SERVER.JS IS RUNNING');
 const port = process.env.PORT || 3000;
 const db = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASS);
 
-try {
-  console.log('Connecting to MongoDB...');
+async function connectDB() {
+  try {
+    console.log('Connecting to MongoDB...');
 
-  await mongoose.connect(db);
+    await mongoose.connect(db);
 
-  console.log('✅ MongoDB connected');
-} catch (err) {
-  console.error('❌ MongoDB connection failed');
-  console.error(err);
+    console.log('✅ MongoDB connected');
+  } catch (err) {
+    console.error('❌ MongoDB connection failed');
+    console.error(err);
+  }
 }
+
+connectDB();
 
 const server = app.listen(port, () =>
   console.log(`Server is listening to request on port ${port}`),
